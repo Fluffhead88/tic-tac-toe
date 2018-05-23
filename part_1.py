60fdb2c9906d67bb80ff4b162ce86674155a13b6
Fluffhead88 <thigpen.zach@gmail.com>
2018-05-22 23:54:36 -0400

    Added move function but not working

--- part_1.py
+++ part_1.py
@@ -1,8 +1,4 @@
-#def CheckPosition(x):
-#    if(board[x] == ' '):
-#        return True
-#    else:
-#        return False
+
 
 class Board:
     def __init__(self):
@@ -15,28 +11,35 @@ class Board:
         print ("---+---+---")
         print (self.space[7], " |", self.space[8], "|", self.space[9])
 
+    def free_space(self):
+        if self.space == " ":
+            return True
 
-
-class Player:
-    def __init__(self):
-        self.xplayer = "X"
-        self.oplayer = "O"
-
-        for char in Board():
-            if char == " "
-                return True
-            else:
-                False
-        while True:
-            
-        place = int(input("Pick a spot on the board between 1-9 > "))
+    def move(self):
+        print("Choose a place between 1-9 > ")
+        place = int(input())
         self.space.append(place)
+        return place
+    
+    def win(self):
+        possible_wins = ([1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7])
+
+"""class Player:
+    def __init__(self):
+
+            place = int(input("Pick a spot on the board between 1-9 > "))
+            self.space.append(place)
+        return place"""
 
 
-    """def make_move(self):
-        player = input(int())
-        if self.space = " "
-            self.space.append(player)"""
+
+
+
+
+"""def make_move(self):
+    player = input(int())
+    if self.space = " "
+        self.space.append(player)"""
 
 
 
@@ -46,6 +49,6 @@ class Player:
 #x_move = int(input("Please choose a space number. > "))
 
 
-
 board = Board()
 board.draw()
+board.move()
